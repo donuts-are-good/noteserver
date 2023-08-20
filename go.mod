8217e363672e92bb1490a91beada24d9132af7e4
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-08-19 22:02:53 -0500

    up and running

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/donuts-are-good/noteserver
 
 go 1.21.0
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.17
+)
--- main.go
+++ main.go
@@ -1,5 +1,69 @@
 package main
 
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Note struct {
+	ID    int    `json:"id"`
+	Date  string `json:"date"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+var db *sql.DB
+
+func initDatabase() {
+	var err error
+	db, err = sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create table if not exists
+	statement, _ := db.Prepare(`CREATE TABLE IF NOT EXISTS notes 
+		(token TEXT, id INTEGER PRIMARY KEY, date TEXT, title TEXT, body TEXT)`)
+	statement.Exec()
+}
+
+func getNotes(w http.ResponseWriter, r *http.Request) {
+	token := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+
+	notes := []Note{}
+
+	rows, err := db.Query("SELECT id, date, title, body FROM notes WHERE token = ?", token)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var note Note
+		if err := rows.Scan(&note.ID, &note.Date, &note.Title, &note.Body); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		notes = append(notes, note)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(notes)
+}
+
 func main() {
-	println("alive!")
+	initDatabase()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/pull", getNotes).Methods("GET")
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
